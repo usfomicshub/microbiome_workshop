82b0fac349ac21b65abc43c087814d73046855e4
J Oberstaller <joberstaller@gmail.com>
2020-03-10 13:53:42 -0400

    added source-files and TEK's picrust tutorial

---
+++ R/Rsource/Hub_microbiome_workshop_source.R
@@ -0,0 +1,44 @@
+# 3/10/2020: source-script for setting up environment to run the USF Omics Hub Microbiome Data-Analysis Workshop pipeline(s).
+# Author: J. Oberstaller
+
+# install necessary general packages/dependencies if they aren't already installed (combining AS, JG, JO, and TEK package-requirements)
+packages <- c("BiocManager","knitr", "caTools", "ggpubr", "dplyr", "RColorBrewer", "reshape2", "ggplot2","hrbrthemes","gcookbook","RCurl","fs", "readr")
+if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
+  install.packages(setdiff(packages, rownames(installed.packages())),repos='https://cloud.r-project.org') 
+}
+
+# install our primary analysis-packages from bioconductor if they aren't already installed
+bioconductor.packages <- c("dada2", "microbiome", "phyloseq", "ALDEx2")
+if (length(setdiff(bioconductor.packages, rownames(installed.packages()))) > 0){
+  BiocManager::install(c("dada2", "microbiome", "phyloseq", "ALDEx2"),
+                       ask = FALSE,
+                       quiet = TRUE,
+                       verbose = FALSE)
+}
+
+# clean up leftover variables from above
+rm("packages", "bioconductor.packages")
+
+
+# load required packages (AS)
+library(dada2)
+library(RCurl)
+library(fs)
+library(dplyr)
+
+# load required packages (JG)
+library(microbiome)
+library(phyloseq)
+library(knitr)
+library(ggpubr)
+library(dplyr)
+library(RColorBrewer)
+library(reshape2)
+library(ggplot2)
+library(hrbrthemes)
+library(gcookbook)
+
+# load required packages (TEK)
+library(ALDEx2)
+library(readr)
+library(dplyr)
