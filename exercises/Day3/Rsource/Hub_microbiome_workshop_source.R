55392a4ce8a2246462ac51f686f776d692f5de16
oberstal <joberstaller@gmail.com>
2020-06-09 22:45:38 -0400

    added .Rmd with WORKING Day3 source-install chunk

--- exercises/Day3/Rsource/Hub_microbiome_workshop_source.R
+++ exercises/Day3/Rsource/Hub_microbiome_workshop_source.R
@@ -19,47 +19,86 @@ packages <- c("BiocManager",
               "simEd",
               "tictoc",
               "vegan"
-             )
+)
 if (length(setdiff(packages,
                    rownames(installed.packages()))) > 0) {
   install.packages(setdiff(packages,
                            rownames(installed.packages())),
-                   repos='https://cloud.r-project.org') 
+                   repos = 'https://cloud.r-project.org',
+                   type = "binary",
+                   update = TRUE,
+                   #ask = FALSE,
+                   verbose = TRUE
+  ) 
 }
 
 # install our primary analysis-packages from bioconductor if they aren't already installed
-bioconductor.packages <- c("dada2",
-                           "microbiome",
+bioconductor.packages <- c("microbiome",
                            "phyloseq",
-                           "ALDEx2",
                            "SIAMCAT",
-                           "metagenomeSeq")
+                           "metagenomeSeq",
+                           "dada2")
 if (length(setdiff(bioconductor.packages, rownames(installed.packages()))) > 0){
-  BiocManager::install(c("dada2",
-                         "microbiome",
+  BiocManager::install(c("microbiome",
                          "phyloseq",
-                         "ALDEx2",
                          "SIAMCAT",
-                         "metagenomeSeq"),
-                       ask = FALSE,
-                       quiet = TRUE,
-                       verbose = FALSE)
+                         "metagenomeSeq",
+                         "dada2"),
+                       type = "binary",
+                       update = FALSE,
+                       #ask = FALSE,
+                       verbose = TRUE
+  )
+}
+
+# now install this one separately as there is no binary available for this version of R yet (GenomeInfoDb is a dependency of ALDEx2)
+bioconductor.packages2 <- c("GenomeInfoDb","ALDEx2")
+
+if (length(setdiff(bioconductor.packages2, rownames(installed.packages()))) > 0){
+  BiocManager::install(c("GenomeInfoDb", "ALDEx2"),
+                       type = "both",
+                       update = FALSE,
+                       #ask = FALSE,
+                       verbose = TRUE
+  )
 }
 
 # clean up leftover variables from above
-rm("packages", "bioconductor.packages")
+rm(list=ls())
+#rm("packages", "bioconductor.packages")
+
+# make required directories if they don't already exist
+makeRdirs <- function () {
+  # make ./Routput directory if it doesn't already exist
+  mainDir = getwd()
+  subDir = "/Routput"
+  
+  dir.create(paste(mainDir,
+                   subDir,
+                   sep = ""),
+             showWarnings = FALSE)
+  
+  # make ./Rfigs directory if it doesn't exist
+  subDir2 = "/Rfigs"
+  dir.create(paste(mainDir,
+                   subDir2,
+                   sep = ""),
+             showWarnings = FALSE)
+}
+
+makeRdirs()
 
 # add these load-package parts to the markdown for the appropriate day of the course instead of this source-document
-# load required packages (AS)
-library(dada2)
-library(RCurl)
-library(fs)
-library(dplyr)
-library(GUniFrac)
-library(simEd)
-library(tictoc)
+# load required packages (AS, Day2)
+# library(dada2)
+# library(RCurl)
+# library(fs)
+# library(dplyr)
+# library(GUniFrac)
+# library(simEd)
+# library(tictoc)
 
-# load required packages (JG)
+# load required packages (JG, Day3)
 library(microbiome)
 library(phyloseq)
 library(vegan)
@@ -73,7 +112,7 @@ library(ggplot2)
 library(hrbrthemes)
 library(gcookbook)
 
-# load required packages (TEK)
+# load required packages (TEK, Day3)
 library(ALDEx2)
 library(readr)
 library(dplyr)
